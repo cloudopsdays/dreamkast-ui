57eea7b9179cf98210d2140a403aaaf792e73889
Kazuto Kusama <jacopen@gmail.com>
2021-08-18 11:55:03 +0900

    Fix mobile header

--- src/components/Menu/MobileMenu.tsx
+++ src/components/Menu/MobileMenu.tsx
@@ -10,17 +10,14 @@ type Props = {
 export const MobileMenu: React.FC<Props> = ({ url }) => {
   return (
     <Styled.MobileMenu>
-      <CommonStyled.MenuLink href="/codt2021/ui#booths" rel="noreferrer">
-        <Button style={{ color: '#037f8c' }}>Booths</Button>
-      </CommonStyled.MenuLink>
-      <CommonStyled.MenuLink href="/codt2021/ui/discussionboard" rel="noreferrer">
-        <Button style={{ color: '#037f8c' }}>Discussion</Button>
-      </CommonStyled.MenuLink>
       <CommonStyled.MenuLink href="/codt2021/timetables" rel="noreferrer">
-        <Button style={{ color: '#037f8c' }}>Timetable</Button>
+        <Button style={{ color: '#037f8c' }}>オンデマンドセッション</Button>
+      </CommonStyled.MenuLink>
+      <CommonStyled.MenuLink href="/codt2021/ui#booths" rel="noreferrer">
+        <Button style={{ color: '#037f8c' }}>ブース</Button>
       </CommonStyled.MenuLink>
       <Button href={url} style={{ color: '#037f8c' }}>
-        Logout
+        ログアウト
       </Button>
     </Styled.MobileMenu>
   )
